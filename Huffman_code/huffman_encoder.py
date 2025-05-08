3d671be7346c8a0a864cf84b6d213f1d10b58193
hassan <hassan>
2025-05-08 13:23:20 +0100

    update

--- Huffman_code/huffman_encoder.py
+++ Huffman_code/huffman_encoder.py
@@ -1,3 +1,4 @@
+import math
 def build_huffman_tree(probabilities):
     # Create a list of tuples (character, probability)
     nodes = [(char, prob) for char, prob in probabilities.items()]
@@ -34,7 +35,24 @@ def generate_codes(node, prefix="", codebook=None):
 
     return codebook
 
-# Example usage with given probabilities for each character
+def metrics(probabilities, codes):
+    # Average codeword length:
+    avg_length = sum(probabilities[char] * len(code) for char, code in codes.items())
+    
+    # Entropy:
+    entropy = -sum(p * math.log2(p) for p in probabilities.values())
+    
+    # Efficiency: 
+    efficiency = entropy / avg_length if avg_length > 0 else 0
+    
+    # Redundancy:
+    redundancy = 1 - efficiency
+
+    return avg_length, entropy, efficiency, redundancy
+
+
+
+# Example: 
 probabilities = {
     'a': 0.4,
     'b': 0.3,
@@ -44,10 +62,18 @@ probabilities = {
 
 # Build the Huffman tree
 huffman_tree = build_huffman_tree(probabilities)
-
+print(huffman_tree)
 # Generate the Huffman codes
 huffman_codes = generate_codes(huffman_tree)
 
 # Display the Huffman codes
 for char, code in huffman_codes.items():
     print(f"{char}: {code}")
+
+#huffman code metrics
+avg, H, eff, red = metrics(probabilities, huffman_codes)
+print("\nMetrics:")
+print(f"  Average Codeword Length: {avg}")
+print(f"  Entropy: {H}")
+print(f"  Efficiency : {eff}")
+print(f"  Redundancy : {red}")
