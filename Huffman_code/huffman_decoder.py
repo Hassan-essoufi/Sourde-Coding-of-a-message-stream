e1deea5540bc7212182abc5a93dfe0f3889d54f5
hassan <hassan>
2025-05-09 16:04:55 +0100

    update

--- Huffman_code/huffman_decoder.py
+++ Huffman_code/huffman_decoder.py
@@ -1,19 +1,13 @@
 import random
-
 def decode_huffman_with_noise(encoded_str, huffman_codes, noise_prob=0.05):
     reverse_huffman_codes = {code: char for char, code in huffman_codes.items()}
     decoded_str = ''
     current_code = ''
-    
-
     def add_noise(bit):
         return '1' if bit == '0' and random.random() < noise_prob else '0' if bit == '1' and random.random() < noise_prob else bit
-    
-
     for bit in encoded_str:
         bit_with_noise = add_noise(bit)  
         current_code += bit_with_noise  
-        
         if current_code in reverse_huffman_codes:
             decoded_str += reverse_huffman_codes[current_code]  
             current_code = ''  
@@ -21,7 +15,6 @@ def decode_huffman_with_noise(encoded_str, huffman_codes, noise_prob=0.05):
             closest_code = min(reverse_huffman_codes.keys(), key=lambda code: hamming_distance(current_code, code))
             decoded_str += reverse_huffman_codes[closest_code]
             current_code = '' 
-
     return decoded_str
 
 def hamming_distance(str1, str2):
