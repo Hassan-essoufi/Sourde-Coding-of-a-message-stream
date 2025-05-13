0e636fe435387270f59d15f0a05f418518a4c510
hassan <hassan>
2025-05-13 21:53:38 +0100

    update

--- Tunstall_coding/test.py
+++ Tunstall_coding/test.py
@@ -22,14 +22,6 @@ print_dictionary(leaves)
 encoded = encode_string(string, leaves)
 print("\nEncoded string (clean):", encoded)
 
-# Add noise
-noisy_encoded = add_gaussian_noise(encoded, sigma=0.5)
-print("Encoded string (noisy):", noisy_encoded)
-
-# Decoding
-decoded = decode_binary(noisy_encoded, leaves)
-print("Decoded string:", decoded)
-
 # tunstall code metrics
 entropy = compute_entropy(probs)
 avg_length = compute_avg_length(leaves)
@@ -38,6 +30,14 @@ print(f"\nEntropy H(X): {entropy:.4f}")
 print(f"Average sequence length: {avg_length:.4f}")
 print(f"Efficiency (H(X)/L): {efficiency:.4f}")
 print(f"Redundancy (R) : {1 - efficiency:.4f}")
+
+# Add noise
+noisy_encoded = add_gaussian_noise(encoded, sigma=0.5)
+print("Encoded string (noisy):", noisy_encoded)
+
+# Decoding
+decoded = decode_binary(noisy_encoded, leaves)
+print("Decoded string:", decoded)
 
 # Decoding accuracy rate
 accuracy = compute_accuracy(string, decoded)
